c721ea9ada915f5f7e14759c34154cd376bbb2fc
Sa-So <60461699+Sa-So@users.noreply.github.com>
2022-02-04 12:09:49 +0530

    Create prob2.py

---
+++ hackerRank/cp-recruitment/prob2.py
@@ -0,0 +1,16 @@
+# the ques was to find prob of picking wednesday
+# but nowhere it was mentioned 
+# if we pick a random day from an year
+# so by default there are 7 days & picking one has 1/7 prob
+print(8) # 1+7
+# do this : https://www.codechef.com/learning/beginnerscamp/register
+# or do dev : 
+# https://www.google.com/search?q=how+to+deploy+your+project&rlz=1C5CHFA_enIN873IN874&oq=how+to+deploy+your+project&aqs=chrome..69i57j0i512j0i22i30.12833j0j7&sourceid=chrome&ie=UTF-8
+# https://www.geeksforgeeks.org/model-view-controllermvc-architecture-for-node-applications/
+# https://auth0.com/blog/modern-full-stack-development-with-nestjs-react-typescript-and-mongodb-part-1/
+# https://www.youtube.com/watch?v=7CqJlxBYj-M
+# https://www.youtube.com/watch?v=vjf774RKrLc
+# https://levelup.gitconnected.com/a-complete-guide-build-a-scalable-3-tier-architecture-with-mern-stack-es6-ca129d7df805
+# https://docs.spring.io/spring-batch/docs/current/reference/html/index-single.html
+# https://www.google.com/search?q=VC%2C+REST+APIs%2C+ORM%2C+SpringBoot%2C+Views%2C+Database+Indexes%2C+Multithreading&rlz=1C5CHFA_enIN873IN874&oq=VC%2C+REST+APIs%2C+ORM%2C+SpringBoot%2C+Views%2C+Database+Indexes%2C+Multithreading&aqs=chrome..69i57.473j0j7&sourceid=chrome&ie=UTF-8
+# or maybe ml ? https://atcoder.jp/contests/ahc001
